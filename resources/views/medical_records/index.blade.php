95dee6ff3afd8cc3c2f16f269002de330f8d8d1c
Hathus <trilogyti@gmail.com>
2024-04-02 13:34:25 -0600

    Cambios en el diseño y logos

--- resources/views/medical_records/index.blade.php
+++ resources/views/medical_records/index.blade.php
@@ -3,9 +3,17 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('medical_records_index.header_title') }}
         </h2>
-        <x-nav-link :href="route('nuevo-expediente')" :active="request()->routeIs('nuevo-expediente')" wire:navigate class="mt-3 border-lime-700">
+        {{-- <x-nav-link :href="route('nuevo-expediente')" :active="request()->routeIs('nuevo-expediente')" wire:navigate class="mt-3 border-lime-700">
             {{ __('Nuevo Expediente') }}
-        </x-nav-link>
+        </x-nav-link> --}}
+        <a href="{{route('nuevo-expediente')}}" class="border border-lime-500 rounded p-2 flex w-1/6 my-2 justify-center items-center text-gray-100 text-sm uppercase font-bold hover:bg-lime-500 hover:text-black">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-6 h-6 mx-1">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            </svg>
+            {{ __('Nuevo Expediente') }}
+        </a>
     </x-slot>
 
     <div class="py-3">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>A la Medida</title>
+    <title>{{ $_ENV['APP_NAME'] }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,11 +18,19 @@
 <body class="font-sans antialiased">
     <div class="text-white bg-gray-900">
         {{-- Fondo de pantalla aquí --}}
-        {{-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> --}}
+        {{-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="/img/xcion 1080x1980_PNG.png" /> --}}
+        <div class="absolute grid grid-cols-1 content-center h-screen w-full">
+            <div class="flex items-center justify-center">
+                <img id="background" src="/img/xcion 1080x1980_PNG.png" alt="logo de la app" />
+            </div>
+        </div>
         <div class="relative flex flex-col items-center justify-between min-h-screen selection:text-white">
             <header class="grid items-center justify-between w-full grid-cols-2 gap-2 p-5 py-8 lg:grid-cols-3 bg-lime-500">
                 <div class="flex text-gray-700 font-bold lg:justify-center lg:col-start-2">
-                    <a href="/">A la Medida</a>
+                    <a href="/" class="flex items-center sm:flex-col">
+                        <img src="/img/xcion 300X300_PNG.png" alt="" width="50px">
+                        <p class="bold md:text-2xl">{{ $_ENV['APP_NAME']}}</p>
+                    </a>
                 </div>
                 @if (Route::has('login'))
                     <livewire:welcome.navigation />
@@ -33,7 +41,7 @@
             </main>
 
             <footer class="w-full py-10 text-sm text-center text-black dark:text-gray/70 bg-lime-500">
-                A la Medida - Todos los derechos reservados - {{ date('Y') }}
+                {{ $_ENV['APP_NAME']}} - Todos los derechos reservados - {{ date('Y') }}
             </footer>
         </div>
     </div>
