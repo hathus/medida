d28e19b562808c55715836f8f849ec87f8efe8dd
Hathus <trilogyti@gmail.com>
2024-04-04 08:08:37 -0600

    archivos de traduccion

---
+++ resources/lang/es/nav-dashboard.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'dashboard' => 'Panel Principal',
+        'medical_records' => 'Expedientes Medicos',
+        'muscle_mass' => 'Calcular Masa Muscular',
+        'profile' => 'Perfil',
+        'logout' => 'Cerrar Sesión',
+    ];
