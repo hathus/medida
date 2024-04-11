9108f658dd64bf7c0d2fc83a08bfddddcdfcc7cd
Hathus <trilogyti@gmail.com>
2024-04-11 16:01:56 -0600

    se condiciono la edición de la consulta

--- resources/views/livewire/show-medical-appointments.blade.php
+++ resources/views/livewire/show-medical-appointments.blade.php
@@ -127,15 +127,21 @@
                             Mostrar
                         </a>
                         {{-- El boton se mostrara solamente si la consulta es igual al día en que se creó --}}
-                        <a href="{{ route('editar-consulta', $consulta->id) }}"
-                            class="border border-blue-700 py-2 px-4 rounded text-gray-100 hover:bg-blue-700 text-xs font-bold uppercase text-center flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-1">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                            </svg>
-                            Editar
-                        </a>
+                        @php
+                            $now = \Carbon\Carbon::now()->format('d m Y');
+                            $appointment = \Carbon\Carbon::parse($consulta->created_at)->format('d m Y');
+                        @endphp
+                        @if ($now === $appointment)
+                            <a href="{{ route('editar-consulta', $consulta->id) }}"
+                                class="border border-blue-700 py-2 px-4 rounded text-gray-100 hover:bg-blue-700 text-xs font-bold uppercase text-center flex items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-1">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+                                Editar
+                            </a>
+                        @endif
                         <button
                             class="border border-red-700 py-2 px-4 rounded text-gray-100 hover:bg-red-700 text-xs font-bold uppercase text-center flex items-center justify-center"
                             wire:click="$dispatch('deleteAlertMedicalApp', {{ $consulta->id }})">
