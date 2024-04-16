e625bb53b26f82804aabe184f95f7f607bb088dd
Hathus <trilogyti@gmail.com>
2024-04-16 10:41:45 -0600

    corrección de la navegación

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="-mx-3 flex flex-1 justify-end">
     @auth
-        <a href="{{ url('recetas') }}"
+        <a href="{{ route('recetas') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-gray-700 dark:hover:text-white/80 dark:focus-visible:ring-white">
             Arma tu menu
         </a>
