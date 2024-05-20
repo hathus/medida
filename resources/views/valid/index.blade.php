2196309f85188eceba5fc5cff98064df4a6cd35a
Hathus <trilogyti@gmail.com>
2024-05-20 09:46:02 -0600

    validado por imagenes

--- resources/views/valid/index.blade.php
+++ resources/views/valid/index.blade.php
@@ -5,6 +5,10 @@
         </h2>
     </x-slot>
 
+    @php
+        $alfonso_title = "¡Una Herramienta Web Enfocada a la Productividad!";
+        $alfonso_op = "Es una herramienta enfocada a reducir los tiempos y movimientos al momento de realizar una consulta médica, ayudando no solo a agilizar el flujo de atención a los pacientes, sino que también ayuda a la digitalización de los expedientes médicos y a llevar una registro de las consultas realizadas, otra de las grandes ventajas es la evaluación del consumo calórico y la recomendación de la alimentación requerida con menús detallados.";
+    @endphp
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid sm:grid-cols-2">
@@ -17,25 +21,17 @@
                 </div>
 
                 <div class="rounded w-full p-2 flex items-end relative">
+                    <img class="rounded" src="img/damian.jpg" alt="damian">
+                    <button onclick="opinion('damian')"
+                        class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
+                        Opinión
+                    </button>
+                </div>
+
+
+                <div class="rounded w-full p-2 flex items-end relative">
                     <img class="rounded" src="img/torres.jpg" alt="torres">
                     <button onclick="opinion('torres')"
-                        class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
-                        Opinión
-                    </button>
-                </div>
-
-
-                <div class="rounded w-full p-2 flex items-end relative">
-                    <img class="rounded" src="img/frank.jpg" alt="frank">
-                    <button onclick="opinion('frank')"
-                        class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
-                        Opinión
-                    </button>
-                </div>
-
-                <div class="rounded w-full p-2 flex items-end relative">
-                    <img class="rounded" src="img/cirenia_b.jpg" alt="cirenia_b">
-                    <button onclick="opinion('cirenia_b')"
                         class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
                         Opinión
                     </button>
@@ -50,16 +46,32 @@
                 </div>
 
                 <div class="rounded w-full p-2 flex items-end relative">
-                    <img class="rounded" src="img/damian.jpg" alt="damian">
-                    <button onclick="opinion('damian')"
+                    <img class="rounded" src="img/frank.jpg" alt="frank">
+                    <button onclick="opinion('frank')"
                         class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
                         Opinión
                     </button>
                 </div>
 
                 <div class="rounded w-full p-2 flex items-end relative">
-                    <img class="rounded" src="img/serafin.jpg" alt="serafin">
-                    <button onclick="opinion('serafin')"
+                    <img class="rounded" src="img/cirenia.jpg" alt="cirenia">
+                    <button onclick="opinion('cirenia')"
+                        class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
+                        Opinión
+                    </button>
+                </div>
+
+                <div class="rounded w-full p-2 flex items-end relative">
+                    <img class="rounded" src="img/edgar.jpg" alt="edgar">
+                    <button onclick="opinion('edgar')"
+                        class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
+                        Opinión
+                    </button>
+                </div>
+
+                <div class="rounded w-full p-2 flex items-end relative">
+                    <img class="rounded" src="img/leonel.jpg" alt="leonel">
+                    <button onclick="opinion('leonel')"
                         class="border p-2 border-orange-700 bg-orange-700  text-bold uppercase text-xs sm:w-52 w-32 rounded absolute mx-2 mb-2 hover:bg-orange-600 text-slate-200">
                         Opinión
                     </button>
@@ -78,9 +90,21 @@
 
                     <script>
                         function opinion(name) {
+
+                            switch(name) {
+                                case 'alfonso':
+                                    var title = {{ Js::from( $alfonso_title )}};
+                                    var opinion = {{ Js::from($alfonso_op) }};
+                                    break;
+                                default:
+                                    var title = "No existe un titulo aun";
+                                    var opinion = "No existe una opinion aun";
+                                    break
+                            }
+
                             Swal.fire({
-                                title: "Sweet!",
-                                text: "mi opinión aquí",
+                                title: title,
+                                text: opinion,
                                 imageUrl: "img/" + name + ".jpg",
                                 imageWidth: 500,
                                 imageAlt: name,
